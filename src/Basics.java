0eca95009adbd2660010c47e12c43698f8cf49a1
91934 <91934@Yogeshwari_RPA>
2024-03-18 21:27:36 +0530

    First Commit

---
+++ src/Basics.java
@@ -0,0 +1,17 @@
+public class Basics {
+	public static void main(String arg[]) {
+	System.out.println("Hi Everyone");
+	System.out.print("Good morning");
+	//DataTypes
+	int ticketprice = 1500;
+	boolean paid = true;
+	char Seat = 'U';
+	String Name = "Yogeshwari";
+	System.out.println(ticketprice);
+	System.out.println(paid);
+	System.out.println(Seat);
+	System.out.print("For passanger " + Name + " The ticket price is " + ticketprice + " and the seat position is " + Seat);
+	
+	}
+
+}
